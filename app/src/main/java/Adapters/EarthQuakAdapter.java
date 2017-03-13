caf41481d29fdbf278a74e70bcc00c13678e1533
Piercing <merlosalbarracin@gmail.com>
2017-03-13 21:21:33 +0100

    Circle Background for Magnitude

--- app/src/main/java/Adapters/EarthQuakAdapter.java
+++ app/src/main/java/Adapters/EarthQuakAdapter.java
@@ -1,8 +1,10 @@
 package Adapters;
 
 import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,16 +13,16 @@ import android.widget.TextView;
 
 import com.example.android.quakereport.R;
 
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.regex.Pattern;
 
 import Models.Earthquake;
 
 public class EarthQuakAdapter extends ArrayAdapter<Earthquake> {
 
-    private static Pattern twopart = Pattern.compile("(\\d+),(\\d+)");
+    //private static Pattern twopart = Pattern.compile("(\\d+),(\\d+)");
 
     public EarthQuakAdapter(Context context, ArrayList<Earthquake> earthQuaks) {
         super(context, 0, earthQuaks);
@@ -42,12 +44,25 @@ public class EarthQuakAdapter extends ArrayAdapter<Earthquake> {
         // Find the TextView in the list_earthquake layout with the ID magnitude.
         TextView textViewMagnitude = (TextView) listItemView.findViewById(R.id.magnitude);
 
+        // Set the proper background color on the magnitude circle.
+        // Fetch the background from the TextView, which is a GradientDrawable.
+        GradientDrawable magnitudeCircle = (GradientDrawable) textViewMagnitude.getBackground();
+
+        // Get the appropriate background color based on the current earthquake magnitude
+        int magnitudeColor = getMagnitudeColor(currentEarthquake.getmMagnitude());
+
+        // Set the color on the magnitude circle
+        magnitudeCircle.setColor(magnitudeColor);
+
+        // Format the magnitude to show 1 decimal place
+        String formattedMaginitude = formatMagnitude(currentEarthquake.getmMagnitude());
+
         // Get the magnitude from the current Earthquake object and set this text on the name TextView.
-        assert currentEarthquake != null;
-        textViewMagnitude.setText(currentEarthquake.getmMagnitude());
+        textViewMagnitude.setText(formattedMaginitude);
 
         // Find the TextView in the list_earthquake layout with the ID location_offset.
         TextView textViewLocationOffSet = (TextView) listItemView.findViewById(R.id.location_offset);
+
         // Find the TextView in the list_earthquake layout with the ID location_primary.
         TextView textViewLocationPrimary = (TextView) listItemView.findViewById(R.id.location_primary);
 
@@ -96,6 +111,13 @@ public class EarthQuakAdapter extends ArrayAdapter<Earthquake> {
         return timeFormat.format(dateObject);
     }
 
+    /**
+     * Split the location into two parts from which it finds the word 'of' and assigns it to its views.
+     *
+     * @param location
+     * @param locationOffset
+     * @param locationPrimary
+     */
     private static void splitLocation(String location, TextView locationOffset, TextView locationPrimary) {
 
         String[] parts = location.split("(?<=of)", 2);
@@ -112,8 +134,56 @@ public class EarthQuakAdapter extends ArrayAdapter<Earthquake> {
         try {
             locationOffset.setText(part1);
             locationPrimary.setText(part2);
-        } catch (Exception ex) {
+        } catch (StringIndexOutOfBoundsException ex) {
             ex.getStackTrace();
         }
+    }
+
+    /**
+     * Return the formatted magnitude string showing 1 decimal place (i.e. "3.2")
+     * from a decimal magnitude value.
+     */
+    private String formatMagnitude(double magnitude) {
+        DecimalFormat magnitudeFormat = new DecimalFormat("0.0");
+        return magnitudeFormat.format(magnitude);
+    }
+
+    private int getMagnitudeColor(double magnitude) {
+        int magnitudeColorResourceId;
+        int magnitudeFloor = (int) Math.floor(magnitude);
+        switch (magnitudeFloor) {
+            case 0:
+            case 1:
+                magnitudeColorResourceId = R.color.magnitude1;
+                break;
+            case 2:
+                magnitudeColorResourceId = R.color.magnitude2;
+                break;
+            case 3:
+                magnitudeColorResourceId = R.color.magnitude3;
+                break;
+            case 4:
+                magnitudeColorResourceId = R.color.magnitude4;
+                break;
+            case 5:
+                magnitudeColorResourceId = R.color.magnitude5;
+                break;
+            case 6:
+                magnitudeColorResourceId = R.color.magnitude6;
+                break;
+            case 7:
+                magnitudeColorResourceId = R.color.magnitude7;
+                break;
+            case 8:
+                magnitudeColorResourceId = R.color.magnitude8;
+                break;
+            case 9:
+                magnitudeColorResourceId = R.color.magnitude9;
+                break;
+            default:
+                magnitudeColorResourceId = R.color.magnitude10plus;
+                break;
+        }
+        return ContextCompat.getColor(getContext(), magnitudeColorResourceId);
     }
 }
