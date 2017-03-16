618ab18e8e905bb605c926b5a63e4c7f084625b1
Piercing <merlosalbarracin@gmail.com>
2017-03-16 13:08:14 +0100

    Refactor. App Ok.

--- app/src/main/java/Helpers/QueryUtils.java
+++ app/src/main/java/Helpers/QueryUtils.java
@@ -42,6 +42,12 @@ public final class QueryUtils {
      */
     public static ArrayList<Earthquake> fetchEarthquakeData( String requestUrl ) {
 
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         Log.i( LOG_TAG, "TEST: fetchEarthquakeData() called..." );
 
         // Create URL object.
